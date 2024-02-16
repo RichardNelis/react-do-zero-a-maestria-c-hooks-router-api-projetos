6e22cd2f4f3b2a25bda735c49a780392fdd49e00
Richard Nelis de Souza <souza.richard33@hotmail.com>
2024-02-16 11:25:52 -0300

    Modulo 7 finalizado

--- Modulo 7/requisicoes_http_react/README.md
+++ Modulo 7/requisicoes_http_react/README.md
@@ -1,8 +1,7 @@
-# React + Vite
+Para executar o projeto segue os comandos
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+- npm run server
+    Esse comando server para iniciar a API
 
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+- npm run dev
+    Esse comando server para iniciar a aplicação WEB
--- Modulo 7/requisicoes_http_react/src/App.jsx
+++ Modulo 7/requisicoes_http_react/src/App.jsx
@@ -1,18 +1,23 @@
-import './App.css'
+import "./App.css";
 
 import { useState, useEffect } from "react";
+
+// 4 - Custom Hook
+import { useFetch } from "./hooks/useFetch";
 
 const url = "http://localhost:3000/products";
 
 function App() {
-
   const [products, setProducts] = useState([]);
+
+  // 4 - Custom Hook
+  const { data: items, httpConfig, loading, error } = useFetch(url);
 
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
 
   // 1 - Resgatando dados
-  useEffect(() => {
+  /*useEffect(() => {
     async function fetchData() {
       const response = await fetch(url);
 
@@ -22,43 +27,79 @@ function App() {
     }
 
     fetchData();
-  }, []);
+  }, []);*/
 
   // 2 - Add de Produtos
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const products = {
+    const product = {
       name,
-      price
+      price,
     };
 
-    const response
-  }
+    /*const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(product),
+    });
+
+    // 3 - Carregamento dinâmico
+    const addedProduct = await res.json();
+
+    setProducts((prevProducts) => [...prevProducts, addedProduct]);*/
+
+    // 5 - Refatorando POST
+    httpConfig(product, "POST");
+
+    setName("");
+    setPrice("");
+  };
 
   return (
     <>
       <h1>Lista de Produtos</h1>
-      <ul>
-        {products.map((product) => (
-          <li key={product.id}>{product.name} - R$: {product.price}</li>
-        ))}
-      </ul>
-      <div className='add-product'>
+      {/* 6 - Loading */}
+      {loading && <p>Carregando os dados...</p>}
+      {!error && (
+        <ul>
+          {items?.map((product) => (
+            <li key={product.id}>
+              {product.name} - R$: {product.price}
+            </li>
+          ))}
+        </ul>
+      )}
+      {error && <p>{error}</p>}
+      <div className="add-product">
         <form onSubmit={handleSubmit}>
           <label>
             Nome:
-            <input type="text" value={name} name='name' onChange={(e) => setName(e.target.value)} />
+            <input
+              type="text"
+              value={name}
+              name="name"
+              onChange={(e) => setName(e.target.value)}
+            />
           </label>
           <label>
             Preço:
-            <input type="number" value={price} name='price' onChange={(e) => setPrice(e.target.value)} />
+            <input
+              type="number"
+              value={price}
+              name="price"
+              onChange={(e) => setPrice(e.target.value)}
+            />
           </label>
-          <input type="submit" value="Enviar" />
+          {/*7 - State de Loading no POST*/}
+          {loading && <input type="submit" disabled value="Aguarde" />}
+          {!loading && <input type="submit" value="Enviar" />}
         </form>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
